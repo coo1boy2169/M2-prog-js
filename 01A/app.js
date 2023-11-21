116ba5996cc3a01c0af8577dcd8c125bbf1660a6
Olgun Hristov <36889@ma-web.nl>
2023-11-21 14:47:49 +0100

    veel lets

--- 01A/app.js
+++ 01A/app.js
@@ -2,7 +2,23 @@ class App
 {
     runApplication()
     {
-        console.log("hello world!")
+       console.log("hello world!")
+let appNaam = "Beest"
+let versienummer=   0.9 
+let versiedatum = "21-11-2007"
+let autheur = "paul van loon "
+let copyright = "marshmellow"
+let distributeur= "mr beast"
+let darkmode = true
+
+console.log(appNaam)
+console.log(versienummer)
+console.log(versiedatum)
+console.log(autheur)
+console.log(copyright)
+console.log(distributeur)
+console.log(darkmode)
+
     }
 }
 
