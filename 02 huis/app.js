0eaa248657f8ddd9d719fd052f2549e7781275f7
Coo1boy2169 <>
2023-11-28 15:10:11 +0100

    husi

---
+++ 02 huis/app.js
@@ -0,0 +1,82 @@
+class App
+{
+    runApplication()
+    {
+        let canvas = document.getElementById("canvasId")
+        let g =canvas.getContext("2d")
+      
+        g.fillStyle = "yellow"
+
+g.beginPath()
+        // muur 1 
+        g.moveTo(300,100)
+        g.lineTo(200, 300)
+        g.lineTo(600, 400)
+        g.lineTo(700 , 200)
+        g.lineTo(300,100)
+        g.fill()
+        g.stroke()
+       
+        g.closePath()
+
+
+        // muur 2 
+        g.beginPath()
+        
+        g.fillStyle = "yellow"
+        g.lineTo(200,300)
+        g.lineTo(200, 500)
+        g.lineTo(600,600)
+        g.lineTo(600 , 400)
+        g.lineTo(200 , 300)
+        g.fill()
+        g.stroke()
+       g.closePath()
+
+      
+    
+
+        // driekhoek muur   
+        g.beginPath()
+        g.fillStyle = "yellow" 
+        g.lineTo(600 , 400)
+        g.lineTo(800, 300)
+        g.lineTo( 700, 200)
+        g.lineTo(600, 400)
+        g.fill()
+        g.stroke()
+        g.closePath()
+
+      // muur zijkant 
+      g.beginPath()
+      g.fillStyle = "yellow"
+      g.lineTo(600,400)
+      g.lineTo(600 , 600)
+      g.lineTo(800 , 500)
+      g.lineTo(800 , 300)
+      g.lineTo(600, 400 )
+      g.fill()
+      g.stroke()
+      g.closePath()
+
+      g.beginPath()
+      g.fillStyle = "blue"
+      g.moveTo(500,500)
+      g.lineTo(500,450)
+      g.lineTo(400,420)
+      g.lineTo(400 , 480)
+      g.fill()
+      g.stroke()
+      
+
+
+        console.log(canvas)
+
+      console.log("hello World! ")
+    }
+}
+
+let app = new App()
+app.runApplication()
+
+
