208b97ca14418c98b42aabbc32422539098dc1a5
Coo1boy2169 <>
2023-11-28 14:16:16 +0100

    kleur

--- 02 canvashuis/app.js
+++ 02 canvashuis/app.js
@@ -4,6 +4,10 @@ class App
     {
         let canvas = document.getElementById("canvasId")
         let g =canvas.getContext("2d")
+        g.fillStyle = "red";
+        g.fillRect(0,0,canvas.width,canvas.height);
+
+        g.fillStyle = "black"
         g.fillRect(0,0,10,10);
         console.log(canvas)
       console.log("hello World! ")
