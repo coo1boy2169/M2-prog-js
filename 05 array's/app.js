923f7ac1e42e8422fa2f21c21a1a82ac7b7951e7
Coo1boy2169 <>
2023-12-06 22:35:08 +0100

    arrays

---
+++ 05 array's/app.js
@@ -0,0 +1,27 @@
+class App {
+    runApplication() {
+        let artiesten = ["Bieber", "BlackPink", "LadyGaGa"]
+        let nummers = [2, 5, 7]
+        console.log("hello world!")
+        console.log(artiesten)
+        console.log(nummers)
+
+        for (let i = 0; i + ": "+ ; i++) {
+            let element = artiesten[i];
+            console.log(element)
+        }
+
+
+
+        // vraag hulp 
+        for ( let i =0; i < nummers; i++){
+        let Numbers = nummers[i];
+        console.log(Numbers)
+        }
+
+
+    }
+}
+
+let app = new App()
+app.runApplication()
