13fb4af66f85b50a5d6a9f662a0e7c472d208c38
Coo1boy2169 <>
2023-12-14 10:51:39 +0100

    html array

--- 05 array's/app.js
+++ 05 array's/app.js
@@ -2,6 +2,7 @@ class App {
     runApplication() {
         let artiesten = ["Bieber", "BlackPink", "LadyGaGa"]
         let nummers = [2, 5, 7]
+        let headersByCssClass = document.getElementsByClassName("bandName")
         console.log("hello world!")
         console.log(artiesten)
         console.log(nummers)
@@ -20,7 +21,33 @@ class App {
         }
 
 
+        artiesten.push(" Bob dylan", " prince ")
+
+        for (let i = 0; i < artiesten.length; i++) {
+            let element = artiesten[i];
+            console.log(i + ": " + element)
+        }
+
+        let indexToRemove = artiesten.indexOf("bob dylan")
+
+        artiesten.splice(indexToRemove, 1)
+
+        for (let i = 0; i < headersByCssClass.length; i++) {
+            let Header = headersByCssClass[0].innerText = "nieuws ";
+            headersByCssClass[1].innerText = "reviews";
+            headersByCssClass[2].innerText = "commentaar ";
+            headersByCssClass[3].innerText = " sterren";
+            headersByCssClass[4].innerText = " kritiek";
+            console.log(headersByCssClass[i].innerText)
+        }
+
+
+
+
     }
+
+
+
 }
 
 let app = new App()
