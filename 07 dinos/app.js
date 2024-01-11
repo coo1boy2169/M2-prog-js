ea70b0cc9f5e0f3187b88afa7fc5157c3e04b150
Coo1boy2169 <>
2024-01-11 14:08:28 +0100

    08 op1

--- 07 dinos/app.js
+++ 07 dinos/app.js
@@ -5,12 +5,16 @@ class App {
 
         let dino = new Dino("T-Rex" , true , 69)
         let plantEter = new Dino ("Triceratops" , false , 20)
+        let grass = new Plant("grass"); 
+        let struik = new Plant("struik")
         console.log(dino)
         console.log(plantEter)
 
         dino.eatFood(plantEter)
-        console.log("leeft " +  plantEter.Name +  "? " + plantEter.leeft)
-    }
+        console.log(  "leeft " +  plantEter.Name +  "? " + plantEter.leeft)
+        console.log( "ik ben een " + plantEter.Name + "en ik eet graag " + grass.naam , "maar als er " + struik.naam , "is vind ik het ook niet erg yum")
+        
+    }   
 }
 
 
@@ -25,6 +29,8 @@ class Dino {
         
         console.log("ik ben een "  + this.Name)
         console.log("mijn honger " + this.honger)
+        console.log("ik eet " + this.grass)
+        
     } 
 
 
@@ -34,6 +40,18 @@ class Dino {
         this.leeftijd = leeftijd
         this.leeft = false
         this.honger = false
+    }
+}
+
+
+
+
+class Plant{
+    
+    constructor(naam , leeft){
+        this.naam = naam    
+        this.leeft = leeft 
+
     }
 }
 
